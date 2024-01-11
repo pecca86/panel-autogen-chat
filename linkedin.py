b77a03a8d230a0b81a2271d9e551681d6bf272cb
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2024-01-11 11:36:21 +0100

    sceleton code

--- linkedin.py
+++ linkedin.py
@@ -5,5 +5,9 @@ class LinkedIn:
         self.name = name
         self.url = url
 
+    def _setup_agent():
+        pass
+
     def __str__(self):
         return f'Name: {self.name}\nURL: {self.url}'
+    
